cf3a5729cd8141c64ff80138352c1a12d4c58586
ehi <ehiboy1@gmail.com>
2023-09-10 15:28:36 +0100

    ready for render deploy

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,15 @@ app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 // serve frontend
-if(process.env.NODE_ENV === 'production') {
-   app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-   app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => res.send('please set to production'))
-}
+
+//if(process.env.NODE_ENV === 'production') {
+   app.use(express.static(path.join(__dirname, 'build')));
+
+   //app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+//} else {
+   // app.get('/', (req, res) => res.send('please set to production'))
+//}
 
 app.use(errorHandler)
 
